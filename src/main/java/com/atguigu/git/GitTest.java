a297b00efa92e166ed989d8cc260fd76ccb0723b
chuanyzhong <chuanyzhong@163.com>
2022-01-31 13:22:15 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author chuanyzhong
+ * @create 2022-01-31 13:16
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
